a3af45d4c5372eb09d837f86881cbb52f2efd991
Todor123 <todor_mihov@flutterint.com>
2023-06-02 11:23:37 +0300

    my first commit

---
+++ variables.tf
@@ -0,0 +1,13 @@
+variable "aws_region" {
+  type        = string
+  description = "Region for AWS Resources"
+  default     = "eu-west-1"
+}
+
+variable "policy_arn_SSM" {
+  default = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
+
+variable "policy_arn_Admin_Access" {
+  default = "arn:aws:iam::aws:policy/AdministratorAccess"
+}
